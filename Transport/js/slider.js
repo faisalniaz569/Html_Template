0925d9aaae26e201274a4e6be2e97a9005bd505a
Faisal Niaz <73920072+faisalniaz569@users.noreply.github.com>
2025-11-15 13:14:07 +0600

    Refactor slider.js to use IIFE and strict mode

--- Transport/js/slider.js
+++ Transport/js/slider.js
@@ -1,24 +1,43 @@
 /* Slider */
+/* jslint browser: true */
+/* global document, setTimeout */
 
-let slideIndex = 0;
-showSlides();
+var document, setTimeout;
 
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" slideractive", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " slideractive";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+(function () {
+    "use strict";
 
+    var slideIndex = 0;
+
+    function showSlides() {
+        var i, slides, dots;
+        slides = document.getElementsByClassName("mySlides");
+        dots = document.getElementsByClassName("dot");
+
+        for (i = 0; i < slides.length; i += 1) {
+            slides[i].style.display = "none";
+        }
+
+        slideIndex += 1;
+
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+
+        for (i = 0; i < dots.length; i += 1) {
+            dots[i].className = dots[i].className.replace(" slideractive", "");
+        }
+
+        if (slides[slideIndex - 1]) {
+            slides[slideIndex - 1].style.display = "block";
+        }
+
+        if (dots[slideIndex - 1]) {
+            dots[slideIndex - 1].className += " slideractive";
+        }
+
+        setTimeout(showSlides, 2000);
+    }
+
+    showSlides();
+}());
